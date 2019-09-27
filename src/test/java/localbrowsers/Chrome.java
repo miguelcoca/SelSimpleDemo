c4f3dd245dc9000de93573e410a8ca8588f3827f
Miguel Angel Coca <redridehell@gmail.com>
2019-09-27 16:21:29 -0400

    first commit

---
+++ src/test/java/localbrowsers/Chrome.java
@@ -0,0 +1,33 @@
+package localbrowsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+@Test(groups = {"mac", "windows"})
+public class Chrome {
+    private WebDriver driver;
+
+    @BeforeTest
+    public void chromeSetup(){
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        ChromeOptions options = new ChromeOptions();
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        driver = new ChromeDriver(capabilities);
+    }
+    @Test
+    public void test(){
+        driver.get("http://lazycoder.io/feedback    ");
+        Assert.assertEquals(driver.getTitle(), "Feedback | Lazy Coder IO");
+    }
+
+    @AfterTest
+    public void testTeardown(){
+        driver.quit();
+    }
+}
