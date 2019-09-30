4a8f0ff5e879d687b24c9b39f17ffdf6358c6952
Miguel Angel Coca <redridehell@gmail.com>
2019-09-30 15:26:49 -0400

    couple sample tests added.

--- src/test/java/localbrowsers/Chrome.java
+++ src/test/java/localbrowsers/Chrome.java
@@ -1,6 +1,8 @@
 package localbrowsers;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,9 +11,14 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 @Test(groups = {"mac", "windows"})
 public class Chrome {
     private WebDriver driver;
+
+    public String gTitleLocator=".g-title";
+    public String lButton = "//a[@href='https://seleniumaboveandbeyond.com' and contains(@class,'button')]";
 
     @BeforeTest
     public void chromeSetup(){
@@ -20,10 +27,35 @@ public class Chrome {
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         driver = new ChromeDriver(capabilities);
     }
+
     @Test
     public void test(){
-        driver.get("http://lazycoder.io/feedback    ");
+        driver.get("http://lazycoder.io/feedback");
         Assert.assertEquals(driver.getTitle(), "Feedback | Lazy Coder IO");
+    }
+
+    @Test
+    public void testGTitle(){
+        driver.get("http://lazycoder.io");
+        WebElement gTitle = driver.findElement(By.cssSelector(gTitleLocator));
+        Assert.assertTrue(gTitle.getText().contains("FROM THE BLOG"));
+    }
+
+    @Test
+    public void clickLearnButotn(){
+        driver.get("http://lazycoder.io");
+
+        List<WebElement> buttons = driver.findElements(By.xpath(lButton));
+
+        for(WebElement button:buttons){
+            if(button.getText().contains("LEARN")){
+                //we should interact with the element that matches our criteria.
+                //button.click();
+            }
+
+            button.click();
+            break;
+        }
     }
 
     @AfterTest
