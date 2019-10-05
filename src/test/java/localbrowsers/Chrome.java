7d9e3badc29d0d68e2e8f505ae3329e3931fc819
marcelo perez <marcelo_0614@hotmail.com>
2019-10-04 22:01:20 -0400

    Practica 2 - identifying elements

--- src/test/java/localbrowsers/Chrome.java
+++ src/test/java/localbrowsers/Chrome.java
@@ -17,17 +17,39 @@ import java.util.List;
 public class Chrome {
     private WebDriver driver;
 
-    public String gTitleLocator=".g-title";
+    public String gTitleLocator = ".g-title";
     public String lButton = "//a[@href='https://seleniumaboveandbeyond.com' and contains(@class,'button')]";
+
+    //Login page
+    public String loginURL = "https://github.com/login";
     public String inputUserName = "login_field";
-    public String sUserName = "redridehell@gmail.com";
+    public String sUserName = "marcelo_0614@hotmail.com";
     public String inputPassword = "password";
-    public String sPassword = "noVoyAEscribirMiPasswd";
+    public String sPassword = "xxxxx";
     public String buttonCommit = "//input[@name='commit']";
-    public String ghUserName="(//span[contains(text(),'miguelcoca')])[8]";
+    public String UserInSession = "marcelo0614";
+    public String ghUserName = "(//span[contains(text(),'" + UserInSession + "')])[8]";
+
+    //My Repositories Page
+    public String myRepositoriesURL = "https://github.com/" + UserInSession + ";";
+    public String setStatus = "//div[@class='d-inline-block text-gray-dark']";
+    public String editProfile = "//div[@class='hide-sm hide-md']";
+    public String repositories = "//nav[@class='UnderlineNav-body']/child::a[2]";
+    public String user = "//summary[@aria-label='View profile and more']";
+    public String yourRepositories = "//a[@data-ga-click='Header, go to repositories, text:your repositories']";
+    public String arrayElements[] = {setStatus, editProfile, repositories, user, yourRepositories};
+
+    //Project
+    public String projectURL="https://github.com/miguelcoca/SelSimpleDemo/pull/1/files";
+    public String pullRequest="//a[@class='js-selected-navigation-item selected reponav-item']";
+    public String symbol="//td[@class='blob-num blob-num-expandable']";
+    public String plusSymbol="//span[@data-code-marker='+']";
+    public String arrayElements2[] = {pullRequest, symbol, plusSymbol};
+
+
 
     @BeforeTest
-    public void chromeSetup(){
+    public void chromeSetup() {
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         ChromeOptions options = new ChromeOptions();
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
@@ -35,20 +57,49 @@ public class Chrome {
     }
 
     @Test
-    public void login(){
+    public void login() {
         loginAction();
         Assert.assertNotNull(driver.findElement(By.xpath(ghUserName)));
     }
 
-    private void loginAction() {
-        driver.navigate().to("https://github.com/login");
+    @test
+    public void identifyElementsMyRepos() {
+        result = false
+        int size=arrayElements.length;
+        driver.navigate().to(myRepositoriesURL);
+        for (int i; i <= size; i++) {
+            result = thereIs(arrayElements[i])
+            result = false;
+        }
+    }
+    @test
+    public void identifyElementsHisRepo() {
+        result = false;
+        int size=arrayElements2.length;
+        driver.navigate().to(myRepositoriesURL);
+        for (int i; i <= size; i++) {
+            result = thereIs(arrayElements2[i])
+            System.out.println(result);
+            result = false;
+        }
+    }
+    public boolean thereIs(String a) {
+        boolean isPresent = false;
+        isPresent = driver.findElements(By.xpath(a)).size() > 0;
+        if (isPresent) {
+            System.out.println("Found");
+        } else {
+            System.out.println("No found");
+        }
+        return isPresent;
+    }
 
+    private void loginAction() {
+        driver.navigate().to(loginURL);
         WebElement InputUserName = driver.findElement(By.id(inputUserName));
         InputUserName.sendKeys(sUserName);
-
         WebElement InputPassword = driver.findElement(By.id(inputPassword));
         InputPassword.sendKeys(sPassword);
-
         driver.findElement(By.xpath(buttonCommit)).click();
     }
 
