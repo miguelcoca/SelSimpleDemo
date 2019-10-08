70e7f16f115f62eed857b6a5c66631ae58e08e67
Agustin Colque <agustin.colque@niceincontact.com>
2019-10-08 17:46:27 -0400

    adding status button

--- src/test/java/localbrowsers/Chrome.java
+++ src/test/java/localbrowsers/Chrome.java
@@ -1,6 +1,12 @@
+/*
+(//span[text()='Set status'])[2]
+
+*/
 package localbrowsers;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,9 +15,24 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+
 @Test(groups = {"mac", "windows"})
 public class Chrome {
     private WebDriver driver;
+
+    public String gTitleLocator=".g-title";
+    public String lButton = "//a[@href='https://seleniumaboveandbeyond.com' and contains(@class,'button')]";
+    public String inputUserName = "login_field";
+    public String sUserName = "agustin.colque@gmail.com";
+    public String inputPassword = "password";
+    public String sPassword = "Coflesto7";
+    public String buttonCommit = "//input[@name='commit']";
+    public String ghUserName="(//span[contains(text(),'Maloto7')])[1]";
+    public String bProfile="//a[contains(text(),'Your profile')]";
+    public String bAvatar="(//*[@class='avatar'])[2]";
+    public String bSetStatus="(//span[text()='Set status'])[2]";
+
+
 
     @BeforeTest
     public void chromeSetup(){
@@ -19,11 +40,41 @@ public class Chrome {
         ChromeOptions options = new ChromeOptions();
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         driver = new ChromeDriver(capabilities);
+        driver.manage().window().maximize();
+        loginAction();
     }
+
+    private void loginAction() {
+        driver.navigate().to("https://github.com/login");
+
+        WebElement InputUserName = driver.findElement(By.id(inputUserName));
+        InputUserName.sendKeys(sUserName);
+
+        WebElement InputPassword = driver.findElement(By.id(inputPassword));
+        InputPassword.sendKeys(sPassword);
+
+        driver.findElement(By.xpath(buttonCommit)).click();
+    }
+
+    private void goToProfile() {
+        openUserSetup();
+        driver.findElement(By.xpath(bProfile)).click();
+    }
+
+    private void openUserSetup() {
+        driver.findElement(By.xpath(bAvatar)).click();
+    }
+
     @Test
-    public void testME(){
-        driver.get("http://lazycoder.io/feedback    ");
-        Assert.assertEquals(driver.getTitle(), "Feedback | Lazy Coder IO");
+    public void userName(){
+        goToProfile();
+        Assert.assertNotNull(driver.findElement(By.xpath(ghUserName)));
+    }
+
+    @Test
+    public void setStatus(){
+        goToProfile();
+        Assert.assertNotNull(driver.findElement(By.xpath(bSetStatus)));
     }
 
     @AfterTest
