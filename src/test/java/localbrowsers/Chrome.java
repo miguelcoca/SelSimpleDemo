bb74e80261c13ce8e07d81a7caf7b61a6f174702
Admin_1234* <williamh.gutierrezb@gmail.com>
2019-10-25 17:18:26 -0400

    Practica Final Modulo 1

--- src/test/java/localbrowsers/Chrome.java
+++ src/test/java/localbrowsers/Chrome.java
@@ -34,7 +34,9 @@ package localbrowsers;
 span.blob-code-inner.blob-code-marker[data-code-marker='+']
 
 * */
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -45,6 +47,11 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -53,25 +60,33 @@ public class Chrome {
     private WebDriver driver;
     public String gTitleLocator = ".g-title";
     public String lButton = "//a[@href='https://seleniumaboveandbeyond.com' and contains(@class,'button')]";
-    public String repositorio= "//a[@href='/miguelcoca?tab=repositories']";
+    public String repositorio = "//a[@href='/miguelcoca?tab=repositories']";
 
     public String inputUserName = "login_field";
-    public String sUserName= "williamh.gutierrezb@gmail.com";
-    public String inputPassword= "password";
+    public String sUserName = "williamh.gutierrezb@gmail.com";
+    public String inputPassword = "password";
     public String sPassword = "Admin_1234*";
-    public String buttonCommit="//input[@name='commit']";
-    public String ghUserName ="(//span[contains(text(),'william')])[2]";
+    public String buttonCommit = "//input[@name='commit']";
+    public String ghUserName = "(//span[contains(text(),'will')])[2]";
 
 
-    public String URLsesion = "https://github.com/login";
-    public String URLgit= "https://github.com/williamgutierrez";
-    public String URLdemo="https://github.com/williamgutierrez/SelSimpleDemo";
-    public String expSetStatus ="(//span[contains(text(),'Set status') ])[2]";
-    public String expEditProfile ="(//button[contains(@class,'js-profile-editable-edit-button')])[1]";
+    //public String URLsesion = "https://github.com/login";
+    public String URLgit = "https://github.com/williamgutierrez";
+    public String URLdemo = "https://github.com/williamgutierrez/SelSimpleDemo";
+    public String expSetStatus = "(//span[contains(text(),'Set status') ])[2]";
+    public String expEditProfile = "(//button[contains(@class,'js-profile-editable-edit-button')])[1]";
     public String expRepositories = "//a[contains(text(),'Repositories' )]";
     public String expPullRequeest = "//span[contains(text(),'Pull request')]";
+    // ************************************************
+    public String BaseURLGithub = "https://github.com/";
+    public String paso1 = "(//a[contains(text(), 'Sign')])[2]";
+    public String paso2 = "(//span[contains(text(),'SelSimpleDemo')])[2]//parent::a";
+    public String paso3 = "//a[contains( text(),'localbrowsers')]";
+    public String titulochrome = "//a[text()= 'Chrome.java']";
+    public String row = "//span[@class='file-mode' ]//parent::div";
 
 
+    public String paso4 = "a[href*='Chrome.java']";
 
     @BeforeTest
     public void chromeSetup() {
@@ -81,38 +96,94 @@ public class Chrome {
         driver = new ChromeDriver(capabilities);
     }
 
-
+    @Test
+    public void Github() {
+        driver.navigate().to(BaseURLGithub);
+        String singin = driver.findElement(By.xpath(paso1)).getAttribute("href");
+        esperar(5);
+        loginAction(singin);
+        WebElement Repo = driver.findElement(By.xpath(paso2));
+        Repo.click();
+        GoRepositorio();
+    }
 
     @Test
-    public void met_setStatus(){
-        loginAction();
+    public void GoRepositorio() {
+        esperar(3);
+       String goRepo = driver.findElement(By.xpath(paso3)).getAttribute("href");
+        driver.navigate().to(goRepo);
+
+        WebElement texx = driver.findElement(By.xpath(titulochrome));
+        if (texx.getText().equals("Chrome.java")) {
+            Assert.assertEquals(texx.getText(), "Chrome.java");
+
+            String linkChromejava = texx.getAttribute("href");
+
+            driver.navigate().to(linkChromejava);
+            WebElement rowcodes = driver.findElement(By.xpath(row));
+
+            String cadena = rowcodes.getText();
+            String delimter = " ";
+            String temporal[];
+            temporal = cadena.split(delimter);
+            for (int i = 0; i < temporal.length; i++) {
+                  System.out.println(temporal[i]);
+            }
+
+            driver.findElement(By.xpath("(//button[contains(@class,'btn-octicon')])[3]")).submit();
+            JavascriptExecutor jsexecute = (JavascriptExecutor) driver;
+            WebElement contentCodeEdit = driver.findElement(By.xpath("//div[@class = 'CodeMirror-sizer']"));
+            jsexecute.executeScript("arguments[0].scrollIntoView({block: 'end'});", contentCodeEdit);
+            String TotalrowEdit = driver.findElement(By.cssSelector(".CodeMirror-code>div:last-child>div>div")).getText();
+            Assert.assertEquals(temporal[2], TotalrowEdit);
+            System.out.println("CORRECTO");
+
+
+        } else {
+            System.out.println("ERRORR");
+    }
+
+
+    }
+    public void esperar (int seg) {
+        try {
+            Thread.sleep (seg*1000);
+        } catch (Exception e) {
+  System.out.println("Fallo el cargado");
+        }
+    }
+
+    @Test
+    public void met_setStatus() {
+
         driver.navigate().to(URLgit);
         WebElement titulo = driver.findElement(By.xpath(expSetStatus));
-        boolean paso= titulo.getText().contains("Set status");
+        boolean paso = titulo.getText().contains("Set status");
         Assert.assertTrue(paso);
     }
 
     @Test
-    public void met_setEditProfile(){
-        loginAction();
+    public void met_setEditProfile() {
+        //loginAction();
         driver.navigate().to(URLgit);
         driver.findElement(By.xpath(expEditProfile)).click();
-        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
     public void met_repositories() {
-        loginAction();
+        //loginAction();
         driver.navigate().to(URLgit);
         WebElement repositorio = driver.findElement(By.xpath(expRepositories));
         By repsiLinkLocator = By.linkText(repositorio.getText());
         driver.findElement(repsiLinkLocator).click();
     }
+
     @Test
-    public void met_pullRequest(){
-        loginAction();
+    public void met_pullRequest() {
+        //loginAction();
         driver.navigate().to(URLdemo);
-        WebElement tab_pullrequest= driver.findElement(By.xpath(expPullRequeest));
+        WebElement tab_pullrequest = driver.findElement(By.xpath(expPullRequeest));
         tab_pullrequest.click();
         Assert.assertTrue(tab_pullrequest.getText().contains("Pull requests"));
 
@@ -125,6 +196,7 @@ public class Chrome {
         Assert.assertEquals(driver.getTitle(), "Feedback | Lazy Coder IO");
 
     }
+
     @Test
     public void testGTitle() {
         driver.get("http://lazycoder.io");
@@ -134,14 +206,13 @@ public class Chrome {
     }
 
 
-
     @Test
-    private void loginAction(){
-        driver.navigate().to(URLsesion);
+    private void loginAction(String login) {
+        driver.navigate().to(login);
         WebElement InputUserName = driver.findElement(By.id(inputUserName));
         InputUserName.sendKeys(sUserName);
 
-        WebElement InputPassword=  driver.findElement(By.id(inputPassword));
+        WebElement InputPassword = driver.findElement(By.id(inputPassword));
         InputPassword.sendKeys(sPassword);
 
         driver.findElement(By.xpath(buttonCommit)).click();
@@ -151,13 +222,13 @@ public class Chrome {
 
 
     @Test
-    public void clickLearnButotn(){
+    public void clickLearnButotn() {
         driver.get("https://lazycoder.io/");
 
         List<WebElement> buttons = driver.findElements(By.xpath(lButton));
-        System.out.println(buttons);
-        for(WebElement button:buttons){
-            if(button.getText().contains("LEARN")){
+        System.out.println("boton login click: " + buttons);
+        for (WebElement button : buttons) {
+            if (button.getText().contains("LEARN")) {
                 //we should interact with the element that matches our criteria.
                 button.click();
             }
@@ -168,12 +239,10 @@ public class Chrome {
     }
 
 
-
     @AfterTest
     public void testTeardown() {
         driver.quit();
     }
-	
 
 
 }
